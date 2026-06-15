58fab9e2aa3b1675f5e09c92bfa81cb46f3ece7d
Guilherme0035 <guysilva35x@gmail.com>
2026-06-15 20:46:07 -0300

    chore: configurando application.yml

---
+++ Agenda/Agenda/src/test/java/Secretary_virtual/Agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package Secretary_virtual.Agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
